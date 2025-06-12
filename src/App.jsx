7102d438333927a241028102bb9057ee0a75f25b
swaran21 <saiswaran212005@gmail.com>
2025-06-12 09:38:00 +0530

    building pkce

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import { Button } from "@mui/material"
-import { BrowserRouter as Router,Navigate,Route,Routes,useLocation } from "react-router"
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { setCredentials } from "./store/authSlice";
 
 function App() {
+  const { token, tokenData, logIn, logOut, isAuthenticated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
-    <Router>  
-        <Button variant="contained" color="#dc004r">Login</Button>
+    <Router>
+      {!token ?(
+      <Button
+        variant="contained"
+        color="#dc004e"
+        onClick={() => {
+          logIn();
+        }}
+      >
+        Login
+      </Button>):(
+        <div>
+          <pre>{JSON.stringify(tokenData,null,2)}</pre>
+          <pre>{JSON.stringify(token,null,2)}</pre>
+        </div>
+      )}
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { AuthProvider } from 'react-oauth2-code-pkce';
+import { authConfig } from './authConfig'; // adjust path as needed
 
-import { Provider } from 'react-redux'
-import { store } from './store/store'
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-import App from './App'
-
-// As of React 18
-const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+  <React.StrictMode>
+    <AuthProvider authConfig={authConfig} loadingComponent={<div>Loading....</div>}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
+  </React.StrictMode>
+);
