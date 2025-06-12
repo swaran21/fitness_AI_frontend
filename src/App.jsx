af852d7cd32909f3586d29079c8c48d5898a3611
swaran21 <saiswaran212005@gmail.com>
2025-06-12 06:48:10 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+import { BrowserRouter as Router,Navigate,Route,Routes,useLocation } from "react-router"
+
+function App() {
+  return (
+    <Router>  
+        <Button variant="contained" color="#dc004r">Login</Button>
+    </Router>
+  )
+}
+
+export default App
